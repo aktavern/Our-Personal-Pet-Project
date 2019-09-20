4d5cae885e5311b0237e252306ef05751fc364ba
Ashley Schenker <aktavern@gmail.com>
2019-09-19 19:44:58 -0500

    updated app, notebook, and sql statements

--- app.py
+++ app.py
@@ -6,26 +6,46 @@ import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
 
 #################################################
 # Database Setup
 #################################################
 
-rds_connection_string = "postgres:<PASSWORD>@localhost:5432/austin_animal_center"
+rds_connection_string = "postgres:Felicidad!1@localhost:5432/animals"
 engine = create_engine(f'postgresql://{rds_connection_string}')
 db = SQLAlchemy(app)
+
+# reflect existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine,reflect=True)
+
+# save reference to the table
+Cats = Base.classes.cats
+
+# create our session from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Routes
+#################################################
 
 @app.route("/")
 def index():
     """Return the homepage."""
-    return render_template("index.html")
+    cat_names = session.query(Cats.name).all()
+    # test_dict = dict(test)
+    return jsonify(cat_names)
 
 
 if __name__ == "__main__":
