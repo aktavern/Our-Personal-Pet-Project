c479159c63c40c9d6f4ac5d6226a908431d91a11
Ashley Schenker <aktavern@gmail.com>
2019-09-21 13:37:38 -0500

    IT WORKS

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,3 @@
 const API_KEY = "530a1e36-9d2c-402a-bacb-a9be6f49c46d" 
+
+const MAP_API_KEY = "pk.eyJ1Ijoia2x1Y2FzMTEiLCJhIjoiY2swOXA5dTVzMGI0bzNsbW96d3hzdnFmcyJ9.EyTXB04xD_QxFR-Th_9WYg"
--- static/js/map.js
+++ static/js/map.js
@@ -4,6 +4,9 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
-    accessToken: API_KEY
+    accessToken: MAP_API_KEY
 }).addTo(mymap);
 
+var marker = L.marker([30.267153, -97.743057], {
+    title: "Country of Origin"
+}).addTo(myMap);
